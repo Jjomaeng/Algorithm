2c868e8819b94e2ac7232a11d793c6e2796040a0
ChoMinhee <aezjk12@ewhain.net>
2021-11-23 23:37:42 +0900

    add solution

--- Leetcode15.py
+++ Leetcode15.py
@@ -28,4 +28,54 @@ def find3element(n : list[int]) -> list[int]:
     
     return ans
 
-print(find3element(input))
+
+# brute-force : 타임 아웃 되는 경우
+
+def solution1(nums : list[int]) -> list[list[int]] :
+    results = []
+    nums.sort()
+
+    for i in range(len(nums)) - 2:
+        if i >0  and nums[i] == nums[i-1]:
+            continue
+        for j in range(i+1,len(nums)-1):
+            if j > i + 1 and nums[j] == nums[j - 1] :
+                continue
+            for k in range(j + 1, len(nums)):
+                if k > j + 1 and nums[k] == nums[k-1]:
+                    continue
+                if nums[i] + nums[j] + nums[k] ==0:
+                    results.append((nums[i],nums[j],nums[k]))
+    
+    return results
+
+    # 중복된 값 넘어가는 코드 추가 (계산 시간 더 줄이기)
+
+def solution2(nums:list[int]) -> list[list[int]]:
+    results = []
+    nums.sort()
+
+    for i in range(len(nums)-2):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+        left,right = i+1,len(nums) -1
+        while left <right:
+            sum = nums[i] + nums[left] + nums[right]
+            if sum < 0 :
+                left += 1
+            elif sum > 0:
+                right -= 1
+            else:
+
+                results.append((nums[i],nums[left],nums[right]))
+
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right -1]:
+                    right += 1
+                left += 1
+                right -= 1
+    return results
+
+
+
