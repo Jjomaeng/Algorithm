f6d81c7a1f08c34a2a7392943d073fbab0f1d07c
ChoMinhee <aezjk12@ewhain.net>
2021-12-02 12:18:10 +0900

    add solution

--- Programmers2.py
+++ Programmers2.py
@@ -34,21 +34,22 @@ def my_solution(s:str) -> int:
 
 #solution
 
-def compress(text,tak_len):
-    words = [text[i:i+tak_len] for i in range(0,len(text),tak_len)]
+def compress(text,tok_len):
+    words = [text[i:i+tok_len] for i in range(0,len(text),tok_len)]
     res = []
     cur_word = words[0]
     cur_cnt = 1
-    for a,b in zip(words,words[1:]+['']):
-        if a == b:
+    for a,b in zip(words,words[1:]+[""]):
+        if a==b :
             cur_cnt += 1
         else :
             res.append([cur_word,cur_cnt])
-            cur_word = b
-            cur_cnt = 1
+            cur_word =b
+            cur_cnt =1
     return sum(len(word) + (len(str(cnt)) if cnt > 1 else 0) for word,cnt in res)
 
 def solution(text):
-    return min(compress(text,tak_len) for tak_len in list(range(1,int(len(text)/2)+1))+[len(text)])
+    return(min(compress(text,i) for i in list(range(1,len(text)//2 +1))+[len(text)]))
 
 
+print(solution(input))
