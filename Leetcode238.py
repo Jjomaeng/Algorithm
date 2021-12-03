6026b3c588d1d85b73d7662a4865bf479588dac1
ChoMinhee <aezjk12@ewhain.net>
2021-12-03 15:34:29 +0900

    add solution

--- Leetcode238.py
+++ Leetcode238.py
@@ -19,4 +19,18 @@ def my_solution(input : list[int]) ->list[int]:
     ans = list(map(mul,res))
     return ans
 
-my_solution(input)
+#solution : 
+
+def solution(nums:list[int]) -> list[int]:
+    out = []
+    p =1
+
+    for i in range(0,len(nums)):
+        out.append(p)
+        p = p *nums[i]
+    p = 1
+    for i in range(len(nums)-1,0-1,-1):
+        out[i] = out[i]*p
+        p = p*nums[i]
+    return out
+
