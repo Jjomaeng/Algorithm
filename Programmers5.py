7368b2661acc5f47bf54161cafbe440d41267a49
Jjomaeng <aezjk12@ewhain.net>
2022-04-04 21:52:15 +0900

    add solution

---
+++ Programmers5.py
@@ -0,0 +1,28 @@
+#문제 : 로또의 최고 순위와 최저 순위
+
+def solution(lottos, win_nums):
+    count = 0
+    win = 0
+    for lotto in lottos:
+        if lotto == 0:
+            count += 1
+        elif lotto in win_nums:
+            win += 1
+            win_nums.remove(lotto)
+    if len(win_nums) > count :
+        max_win = 7-count-win
+    else :
+        max_win = 7-len(win_nums) -win
+
+    if win == 0 :
+        low_win = 6
+    else :
+        low_win = 7 - win
+    answer = [max_win,low_win]
+    return answer
+
+lottos = [45, 4, 35, 20, 3, 9]
+
+win_nums = [46, 5, 36, 21, 4, 10]
+
+print(solution(lottos,win_nums))
