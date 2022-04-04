a9c5c329bc4c05cd090487210c28af109a0bf4d4
Jjomaeng <aezjk12@ewhain.net>
2022-04-04 22:04:09 +0900

    add solution

--- Programmers5.py
+++ Programmers5.py
@@ -21,6 +21,17 @@ def solution(lottos, win_nums):
     answer = [max_win,low_win]
     return answer
 
+def solution2(lottos, win_nums):
+
+    rank=[6,6,5,4,3,2,1]
+
+    cnt_0 = lottos.count(0)
+    ans = 0
+    for x in win_nums:
+        if x in lottos:
+            ans += 1
+    return rank[cnt_0 + ans],rank[ans]
+
 lottos = [45, 4, 35, 20, 3, 9]
 
 win_nums = [46, 5, 36, 21, 4, 10]
