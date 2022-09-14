eeae46a388f4448f6e3b87c6a7e4e08bdfac44b0
Jjomaeng <aezjk12@ewhain.net>
2022-09-14 17:41:43 +0900

    add solution

---
+++ Backjoon2011.py
@@ -0,0 +1,21 @@
+MOD = 10 ** 6
+def solution(code,n):
+    dp = [0] * (n+1)
+    if (code[0] == '0'): return 0
+
+    dp[0] = dp[1] = 1
+
+    for i in range(2,n+1):
+        cur = i -1
+        if (code[cur] == '0' and (code[cur - 1] < '1' or code[cur - 1] > '2' )):
+            return 0
+        if (code[cur] != '0') : dp[i] += dp[i - 1]
+        if (code[cur -1 ] == '1' or (code[cur] <= '6' and code[cur -1 ] == '2')) :
+            dp[i] += dp[i -2]
+
+        dp[i] %= MOD
+    answer = dp[n]
+
+    return answer
+
+print(solution("25114",5))
