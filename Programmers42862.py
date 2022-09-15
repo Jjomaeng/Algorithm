43847010ccc0c8a8737360694379186592bd72d5
Jjomaeng <aezjk12@ewhain.net>
2022-09-15 18:00:04 +0900

    add solution

---
+++ Programmers42862.py
@@ -0,0 +1,16 @@
+from re import L
+
+
+def solution(n,lost,reverse):
+    answer = n - len(lost)
+
+    for i in lost:
+        if ( i-1 in reverse ) :
+            reverse.remove(i-1)
+            answer += 1
+        elif (i + 1  in reverse) :
+            reverse.remove(i+1)
+            answer += 1
+    return answer
+
+print(solution(3,[3],[1])) 
